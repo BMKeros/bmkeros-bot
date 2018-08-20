f12a6215d15436c6322d3fa604589757cb922ae0
Israel Lugo <hostelixisrael@gmail.com>
2018-08-20 16:40:04 -0400

    [MOD] refactor move action to folder core

---
+++ src/bot/core/index.js
@@ -0,0 +1,2 @@
+export { default as processInlineQuery } from './processInlineQuery';
+export { default as processMessage } from './processMessage';
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv';
 import TelegramBot from 'node-telegram-bot-api';
-import { processInlineQuery, processMessage } from 'bot/actions';
+import { processInlineQuery, processMessage } from 'bot/core';
 
 // Init config .env
 dotenv.config();
