612e06fc457ffe4ab9ef5642ce70b59b488c7455
Antony Carrizo <antonycarrizo96@gmail.com>
2018-08-24 13:13:19 -0400

    [MOD] Agregada validacion para no agregar dos veces el mismo usuario.

--- src/bot/actions/processStart.js
+++ src/bot/actions/processStart.js
@@ -2,16 +2,33 @@ import { User } from '../../database/models';
 
 const processStart = (bot) => {
     return (message) => {
-        // console.log("processStart", message);
-        User.create({
-            chat_id: '15148651',
-            first_name: 'tony',
-            last_name: 'carrizo',
-            username: 'boanergepro',
-            createdAt: Date.now(),
+        //console.log("processStart", message);
+        let chatId = message.chat.id;
+
+        User.findOne({
+            where: { chat_id: chatId.toString() }
         })
-            .then(res => console.log(res))
-            .catch(err => console.log(err));
+            .then(res => {
+                if (res === null) {
+                    User.create({
+                        chat_id: message.chat.id,
+                        first_name: message.chat.first_name,
+                        last_name: message.chat.last_name,
+                        username: message.chat.username,
+                        //createdAt: Date.now(),
+                    })
+                        .then(res => {
+                            bot.sendMessage(
+                                chatId,
+                                "Bienvenido! Hemos almacenado algunos datos de ti, para que puedas hacer uso del bot inline en cualquier lugar de telegram"
+                            )
+                        })
+                        .catch(err => console.log);
+                } else {
+                    bot.sendMessage(chatId, "Bienvenido de vuelta, nos alegra tenerte por aqui nuevamente.")
+                }
+            })
+            .catch(err => console.log);
     };
 };
 
