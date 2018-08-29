fdccad3ebf77ec983e9b188ede88bd8e1f5c1654
Israel Lugo <hostelixisrael@gmail.com>
2018-08-29 17:47:52 -0400

    [MOD] add method hasMany to model User and replay message to inline keyboard

--- src/index.js
+++ src/index.js
@@ -10,7 +10,22 @@ const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, { polling: true });
 
 bot.on('message', processMessage);
 bot.on('inline_query', processInlineQuery(bot));
+bot.on('callback_query', (m) => {
+    console.log(m);
+    //bot.answerCallbackQuery(m.id, 'Ok, here ya go!');
+    bot.editMessageText(`Group: ${m.data}`, {
+        chat_id: m.message.chat.id,
+        message_id: m.message.message_id,
+        reply_markup: JSON.stringify({
+            inline_keyboard: [[{text: 'Edit Name', callback_data: '..'},{text: 'Add Members', callback_data: '..'}]],
+        }),
+    });
+});
 bot.onText(/\/start/, processStart(bot));
-bot.onText(/\/myGroups/, processGroup(bot));
 bot.on('polling_error', console.log);
 bot.on('webhook_error', console.log);
+
+//Manage Groups
+bot.onText(/\/mygroups/, processGroup(bot));
+bot.onText(/\/groups/, processGroup(bot));
+
