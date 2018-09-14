e4df5a87705bb64a5b6271924ab487f473fad294
Israel Lugo <hostelixisrael@gmail.com>
2018-09-14 16:53:01 -0400

    [ADD] add messages welcome

---
+++ src/messages/welcome.js
@@ -0,0 +1,8 @@
+const newMember = `Bienvenido #{first_name} #{last_name} !! Hemos almacenado algunos datos de ti, para que puedas hacer uso del bot inline en cualquier lugar de telegram.`;
+
+const oldMember = 'Bienvenido de vuelta, nos alegra tenerte por aqui nuevamente.';
+
+export {
+    newMember,
+    oldMember,
+};
